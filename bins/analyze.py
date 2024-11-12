ddd7449d2eddabf540805b45c4345a9a8b0456c4
gogo <gogo246475@gmail.Com>
2024-11-12 23:26:26 +0100

    draw bar

--- bins/analyze.py
+++ bins/analyze.py
@@ -11,7 +11,9 @@ import sys
 import argparse
 
 from src.db_crud.read import read_inputs
+
 from src.analysis_modules.draw_ecdf import draw_image_ecdf
+from src.analysis_modules.draw_means import draw_image_means
 
 def main():
     parser = argparse.ArgumentParser(prog='analyze', description='Understand requests time taken by the program measure in order to detect timing attack vulnerability', epilog='You must be using measure program previously to get data.')
@@ -24,6 +26,7 @@ def main():
 
     values = read_inputs(args.data_base, args.c)
     draw_image_ecdf(values)
+    draw_image_means(values)
 
     print("bye world")
 
